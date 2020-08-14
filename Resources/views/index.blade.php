d130d61a18c6bea3dbb6cfc97a91a1f5036763f0
Guilherme Schuab <guilherme.schuab@gmail.com>
2020-08-14 15:15:40 -0300

    Commit inicial do Módulo

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('projetos::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('projetos.name') !!}
+    </p>
+@endsection
---
+++ Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module Projetos</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/projetos.css') }}"> --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/projetos.js') }}"></script> --}}
+    </body>
+</html>
